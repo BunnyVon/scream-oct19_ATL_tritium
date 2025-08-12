6cb9e64c48d53c8f612a23478485244907ec5885
Sha Feng <sfeng@pnnl.gov>
2025-08-12 10:40:17 -0700

    Initial snapshot (secrets removed; use env vars/template)

---
+++ externals/mam4xx/src/tests/mam4_ndrop_unit_tests.cpp
@@ -0,0 +1,241 @@
+#include "atmosphere_utils.hpp"
+#include "testing.hpp"
+
+#include "mam4xx/aero_modes.hpp"
+#include "mam4xx/conversions.hpp"
+#include <mam4xx/mode_dry_particle_size.hpp>
+
+#include <haero/constants.hpp>
+#include <haero/floating_point.hpp>
+#include <haero/haero.hpp>
+
+#include "mam4xx/conversions.hpp"
+
+#include <catch2/catch.hpp>
+#include <ekat/ekat_pack_kokkos.hpp>
+#include <ekat/logging/ekat_logger.hpp>
+#include <ekat/mpi/ekat_comm.hpp>
+#include <mam4xx/mam4.hpp>
+
+// using namespace haero;
+using namespace mam4;
+using namespace mam4::conversions;
+
+/* Oscar is refactoring get_aer_num
+TEST_CASE("test_get_aer_num", "mam4_ndrop") {
+  ekat::Comm comm;
+  ekat::logger::Logger<> logger("ndrop get aer num unit tests",
+                                ekat::logger::LogLevel::debug, comm);
+
+  int nlev = 1;
+  Real pblh = 1000;
+  Atmosphere atm = mam4::testing::create_atmosphere(nlev, pblh);
+
+  // initialize a hydrostatically balanced moist air column
+  // using constant lapse rate in virtual temperature to manufacture
+  // exact solutions.
+  //
+  // these values correspond to a humid atmosphere with relative humidity
+  // values approximately between 32% and 98%
+  const Real Tv0 = 300;     // reference virtual temperature [K]
+  const Real Gammav = 0.01; // virtual temperature lapse rate [K/m]
+  const Real qv0 =
+      0.015; // specific humidity at surface [kg h2o / kg moist air]
+  const Real qv1 = 7.5e-4; // specific humidity lapse rate [1 / m]
+  init_atm_const_tv_lapse_rate(atm, Tv0, Gammav, qv0, qv1);
+
+  const auto T = atm.temperature;
+  const auto P = atm.pressure;
+
+  auto h_T = Kokkos::create_mirror_view(T);
+  auto h_P = Kokkos::create_mirror_view(P);
+
+  Kokkos::deep_copy(h_T, T);
+  Kokkos::deep_copy(h_P, P);
+
+  mam4::Prognostics progs(nlev);
+  mam4::Diagnostics diags(nlev);
+
+  mam4::AeroConfig mam4_config;
+
+  const auto nmodes = mam4::AeroConfig::num_modes();
+
+  // initialize progs
+  const Real number_mixing_ratio = 2e7;
+  const Real mass_mixing_ratio = 3e-8;
+  for (int m = 0; m < nmodes; m++) {
+    Kokkos::deep_copy(progs.n_mode_i[m], number_mixing_ratio);
+    Kokkos::deep_copy(progs.n_mode_c[m], number_mixing_ratio);
+    for (int aid = 0; aid < 7; ++aid) {
+      const int s = aerosol_index_for_mode(static_cast<ModeIndex>(m),
+                                           static_cast<AeroId>(aid));
+      if (s >= 0) {
+        auto h_q_view_i = Kokkos::create_mirror_view(progs.q_aero_i[m][s]);
+        auto h_q_view_c = Kokkos::create_mirror_view(progs.q_aero_c[m][s]);
+        for (int k = 0; k < nlev; ++k) {
+          h_q_view_i(k) = mass_mixing_ratio;
+          h_q_view_c(k) = mass_mixing_ratio;
+        }
+        Kokkos::deep_copy(progs.q_aero_i[m][s], h_q_view_i);
+        Kokkos::deep_copy(progs.q_aero_c[m][s], h_q_view_c);
+      }
+    }
+  }
+
+  // initialize diags
+  for (int m = 0; m < nmodes; m++) {
+    Real dry_vol = 0.0;
+    for (int aid = 0; aid < 7; ++aid) {
+      const int s = aerosol_index_for_mode(static_cast<ModeIndex>(m),
+                                           static_cast<AeroId>(aid));
+      if (s >= 0) {
+        dry_vol += mass_mixing_ratio / aero_species(s).density;
+      }
+    }
+  }
+
+  // Call mam4xx kernel across all modes, levels
+  Kokkos::parallel_for(
+      "compute_dry_particle_size", nlev, KOKKOS_LAMBDA(const int i) {
+        mode_avg_dry_particle_diam(diags, progs, i);
+      });
+
+  mam4::Real naerosol[nmodes];
+
+  // iterate over modes and columns to calculate aer num
+  for (int m = 0; m < nmodes; m++) {
+    auto h_diam_total =
+        Kokkos::create_mirror_view(diags.dry_geometric_mean_diameter_total[m]);
+    Kokkos::deep_copy(h_diam_total, diags.dry_geometric_mean_diameter_total[m]);
+
+    for (int k = 0; k < nlev; k++) {
+      logger.info("k = {}, m = {}", k, m);
+      Real vaerosol = conversions::mean_particle_volume_from_diameter(
+          h_diam_total(k), modes(m).mean_std_dev);
+      logger.info("vaerosol = {}", vaerosol);
+
+      Real num2vol_ratio_min =
+          1.0 / conversions::mean_particle_volume_from_diameter(
+                    modes(m).min_diameter, modes(m).mean_std_dev);
+      Real num2vol_ratio_max =
+          1.0 / conversions::mean_particle_volume_from_diameter(
+                    modes(m).max_diameter, modes(m).mean_std_dev);
+      Real min_bound = vaerosol * num2vol_ratio_max;
+      Real max_bound = vaerosol * num2vol_ratio_min;
+
+      logger.info("atm.temperature = {}, atm.pressure = {}", h_T(k), h_P(k));
+      Real middle = (number_mixing_ratio + number_mixing_ratio) *
+                    conversions::density_of_ideal_gas(h_T(k), h_P(k));
+
+      ndrop::get_aer_num(diags, progs, atm, m, k, naerosol);
+      logger.info("naerosol[{}] = {}", m, naerosol[m]);
+
+      logger.info("min bound = {}, max bound = {}", min_bound, max_bound);
+      logger.info("progs calc = {}", middle);
+
+      // TODO: this test needs to be revisited to run properly on GPU
+      //  come back when this function is being used in a ported
+      //  parameterization
+
+      REQUIRE(
+          FloatingPoint<Real>::in_bounds(naerosol[m], min_bound, max_bound));
+      bool check_calc = FloatingPoint<Real>::equiv(naerosol[m], min_bound) ||
+                        FloatingPoint<Real>::equiv(naerosol[m], middle) ||
+                        FloatingPoint<Real>::equiv(naerosol[m], max_bound);
+      REQUIRE(check_calc);
+    }
+  }
+}
+*/
+
+TEST_CASE("test_explmix", "mam4_ndrop") {
+  ekat::Comm comm;
+  ekat::logger::Logger<> logger("ndrop explmix unit tests",
+                                ekat::logger::LogLevel::debug, comm);
+
+  Real q = -1;
+
+  // set up smoketest values
+  // pretend this is from one level k, in nlev, with adjacent levels k-1 (km1)
+  // and k+1 (kp1)
+  Real qold_km1 = 1;
+  Real qold_k = 1;
+  Real qold_kp1 = 1;
+
+  Real src = 1;
+  Real ek_kp1 = 1;
+  Real ek_km1 = 1;
+  Real overlap_kp1 = 1;
+  Real overlap_km1 = 1;
+
+  Real dt = .1;
+  bool is_unact = false;
+
+  Real qactold_km1 = 1;
+  Real qactold_kp1 = 1;
+
+  ndrop::explmix(qold_km1, qold_k, qold_kp1, q, src, ek_kp1, ek_km1,
+                 overlap_kp1, overlap_km1, dt, is_unact);
+
+  logger.info("q = {}", q);
+  REQUIRE(FloatingPoint<Real>::equiv(q, 1.1));
+
+  is_unact = true;
+
+  ndrop::explmix(qold_km1, qold_k, qold_kp1, q, src, ek_kp1, ek_km1,
+                 overlap_kp1, overlap_km1, dt, is_unact, qactold_km1,
+                 qactold_kp1);
+
+  logger.info("q = {}", q);
+  REQUIRE(FloatingPoint<Real>::equiv(q, 0.9));
+}
+
+TEST_CASE("test_maxsat", "mam4_ndrop") {
+  ekat::Comm comm;
+  ekat::logger::Logger<> logger("ndrop maxsat unit tests",
+                                ekat::logger::LogLevel::debug, comm);
+
+  logger.info("start of maxsat test");
+  int nmodes = AeroConfig::num_modes();
+  logger.info("nmodes = {}", nmodes);
+
+  Real zeta = 0;
+  Real eta[nmodes];
+  Real smc[nmodes];
+  Real smax = 0;
+
+  // set up smoketest values
+  for (int m = 0; m < nmodes; m++) {
+    eta[m] = 0;
+    smc[m] = 1;
+  }
+
+  ndrop::maxsat(zeta, eta, nmodes, smc, smax);
+  logger.info("smax = {}", smax);
+  REQUIRE(FloatingPoint<Real>::equiv(smax, 1e-20));
+
+  for (int m = 0; m < nmodes; m++) {
+    smc[m] = 0;
+  }
+  smax = 0;
+  ndrop::maxsat(zeta, eta, nmodes, smc, smax);
+  logger.info("smax = {}", smax);
+  REQUIRE(FloatingPoint<Real>::equiv(smax, 1e-10));
+
+  // set up smoketest values
+  for (int m = 0; m < nmodes; m++) {
+    eta[m] = 1;
+    smc[m] = 1;
+  }
+  smax = 0;
+  Real double_answer = 0.4698982925962298;
+  Real single_answer = 0.46989828;
+
+  ndrop::maxsat(zeta, eta, nmodes, smc, smax);
+  logger.info("smax = {}", smax);
+  logger.info("double_answer = {}, single_answer = {}", double_answer,
+              single_answer);
+  bool test = FloatingPoint<Real>::equiv(smax, double_answer) ||
+              FloatingPoint<Real>::equiv(smax, single_answer);
+  REQUIRE(test);
+}
---
+++ runscript/run_WNP_ICRUELM_perlmutter_historical_R2_Feb2015AR.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+# Set run options
+#=============================================
+
+resolution=WNP_ne1024pg2_r0125_oRRS18to6v3
+compset=ICRUELM
+checkout_date=20220926  #the date you *checked out* the code
+branch=ad0e203          #actual git hash of branch to check out
+run_descriptor=SCREAMv0 #will be SCREAMv0 for production run
+repo=scream
+machine=pm-cpu
+compiler=gnu
+project=m4393
+stop_option="nmonths"
+stop_n="36"
+rest_n="1"
+walltime="2:00:00"
+queue="regular"
+debug_compile='FALSE'
+date_string=`date +"%Y%m%d%H%M"`
+
+# Setup processor layout
+nnodes_atm=40
+nnodes_ocn=40
+nthreads=1
+mpi_tasks_per_node=128
+ntasks_atm=$(expr ${nnodes_atm} \* ${mpi_tasks_per_node})
+ntasks_ocn=$(expr ${nnodes_ocn} \* ${mpi_tasks_per_node})
+total_tasks_per_node=$(expr ${mpi_tasks_per_node} \* ${nthreads})
+if [ ${ntasks_ocn} -ne ${ntasks_atm} ]; then
+    nnodes=$(expr ${nnodes_atm} + ${nnodes_ocn})
+else
+    nnodes=${nnodes_atm}
+fi
+pelayout=${nnodes}x${mpi_tasks_per_node}x${nthreads}
+
+stridc=16
+npcpl=$(expr ${ntasks_atm} \/ ${stridc})
+echo "npcpl=${npcpl}"
+
+# Who to send email updates on run status to
+email_address="sourav.taraphdar@pnnl.gov"
+
+# Set flags specific to running this script
+do_download=false
+do_newcase=true
+do_setup=true
+do_build=true
+do_submit=true
+
+case_name=SCREAMv0.${compset}.${resolution}.${date_string}
+
+# Set paths
+code_root=/global/cfs/cdirs/m1867/souravt/scream-11apr23
+case_root=$SCRATCH/E3SM/cases/${case_name}
+
+######################################################################################
+### USERS PROBABLY DON'T NEED TO CHANGE ANYTHING BELOW HERE EXCEPT user_nl_* FILES ###
+######################################################################################
+
+# Make directories created by this script world-readable:
+#=============================================
+umask 022
+
+# Download code
+#=============================================
+if [ "${do_download}" == "true" ]; then
+
+    echo "Cloning repository repo = $repo into branch = $branch under code_root = $code_root"
+    cdir=`pwd`
+    mkdir -p $code_root/
+
+    # This will put repository, with all code, in directory $tag_name
+    git clone git@github.com:E3SM-Project/${repo}.git $code_root
+    
+    # Setup git hooks
+    rm -rf $code_root/.git/hooks
+    git clone git@github.com:E3SM-Project/E3SM-Hooks.git $code_root/.git/hooks
+    cd $code_root
+    git config commit.template $code_root/.git/hooks/commit.template
+
+    # Bring in all submodule components
+    git submodule update --init --recursive
+
+    # Check out desired branch
+    git checkout ${branch}
+    cd ${cdir}
+
+fi
+
+# Create case
+#=============================================
+if [ "${do_newcase}" == "true" ]; then
+	    ${code_root}/cime/scripts/create_newcase \
+        --case ${case_root} --compset ${compset} --res ${resolution} --machine ${machine} --compiler ${compiler} \
+        --project ${project} --queue ${queue} --walltime ${walltime}
+fi
+
+# Copy this script to case directory
+#=============================================
+cp -v `basename $0` ${case_root}/
+
+# Setup
+#=============================================
+if [ "${do_setup}" == "true" ]; then
+    cd ${case_root}
+
+    # Set run length
+    ./xmlchange STOP_OPTION=${stop_option},STOP_N=${stop_n}
+    ./xmlchange REST_N=${rest_n}
+
+    # Set processor layout
+    if [ ${ntasks_ocn} -ne ${ntasks_atm} ]; then
+        ./xmlchange NTASKS=${ntasks_ocn}
+        ./xmlchange NTASKS_ATM=${ntasks_atm}
+        ./xmlchange ROOTPE_ATM=${ntasks_ocn}
+    else
+        ./xmlchange NTASKS=${ntasks_atm}
+    fi
+    ./xmlchange NTHRDS_ATM=${nthreads}
+    ./xmlchange MAX_MPITASKS_PER_NODE=${mpi_tasks_per_node}
+    ./xmlchange NTASKS_CPL=${npcpl} # change tasks in CPL if using strid
+    ./xmlchange PSTRID_CPL=${stridc}
+    ./xmlchange NTHRDS=1 # set all to 1 first
+    ./xmlchange MAX_TASKS_PER_NODE=${total_tasks_per_node}
+
+    # Flag for debug compile
+    ./xmlchange --id DEBUG --val ${debug_compile}
+    
+    ./xmlchange PIO_NETCDF_FORMAT="64bit_data"
+    ./xmlchange PIO_VERSION="2"
+    ./xmlchange PIO_BUFFER_SIZE_LIMIT=134217728
+
+    ./xmlchange DATM_MODE=CLMCRUNCEPv7
+    ./xmlchange DATM_CLMNCEP_YR_START=2012,DATM_CLMNCEP_YR_END=2015,DATM_CLMNCEP_YR_ALIGN=2012
+    ./xmlchange RUN_STARTDATE=2012-01-31
+    ./xmlchange START_TOD=0
+
+    cat <<EOF >> user_nl_elm
+    fsurdat = '/pscratch/sd/s/souravt/RRM_setup_tutorial/RRM_generation_inputs/surfdata_WNP_ne128x8pg2_simyr2010_c230411.nc'
+    finidat = '/global/cfs/cdirs/m1867/souravt/r0125.elm.r.0051-01-01-00000.nc'
+EOF
+
+    # Finally, run setup
+    ./case.setup
+
+    # The run location is determined in the bowels of CIME
+    # Symlink to that location from user-chosen $case_root (=current dir)
+    ln -s `./xmlquery -value RUNDIR` run
+
+
+    # This disables the logic that sets tprof_n and tprof_options internally.
+    #./xmlchange --file env_run.xml TPROF_TOTAL=-1
+    #echo "tprof_n = 1" >> user_nl_cpl
+    #echo "tprof_option = 'nsteps'" >> user_nl_cpl
+fi
+
+# Build
+#=============================================
+if [ "${do_build}" == "true" ]; then
+    cd ${case_root}
+    ./case.build
+fi
+
+# Run
+#=============================================
+if [ "${do_submit}" == "true" ]; then
+    cd ${case_root}
+
+    # Set file striping on run dir for writing large files
+
+#    ./xmlchange  -file env_batch.xml -id JOB_WALLCLOCK_TIME -val '0:30:00'
+#    ./xmlchange  -file env_batch.xml -id JOB_QUEUE          -val 'debub'
+    ./case.submit --batch-args="--mail-type=ALL --mail-user=${email_address}"
+fi
+
+echo "Done working in ${case_root}"
